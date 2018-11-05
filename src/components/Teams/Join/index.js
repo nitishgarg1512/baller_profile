dfaafc35173a3169244b4ced394aba104a121257
nitish.garg1512 <nitish.garg1512@gmail.com>
2018-11-05 22:23:22 +0700

    some changes

--- src/components/Teams/Join/index.js
+++ src/components/Teams/Join/index.js
@@ -86,6 +86,7 @@ class Join extends React.Component {
     const { navigation, createRelationship, authUser, authPlayer } = this.props;
     const { players, team } = this.state;
 
+
     return (
       <Container>
         <Content>
@@ -105,42 +106,45 @@ class Join extends React.Component {
             </UppercasedText>
           </View>
           <View style={[{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }]}>
-            {players && players.map(player => (
-              <TouchableOpacity key={player.id} style={{ paddingTop: 15, paddingBottom: 15, flex: 1, borderRightWidth: 0.6, borderRightColor: 'rgba(0,0,0,.3)' }} onPress={() => navigation.navigate(paths.client.ProfilesView, { id: player.id })}>
-                <View style={styles.displayFlexCenterColumn}>
-                  <View style={styles.TeamsJoinCardImage}>
-                    <Thumbnail style={styles.joinTeamCardImage} source={player.user.profile_pic ? { uri: player.user.profile_pic } : images.user} />
-                  </View>
-                  <Text style={[styles.playerNameText, styles.py10]}>
-                    {player.user.first_name}
-                    {' '}
-                    {player.user.last_name}
-                    {navigation.getParam('team').captain === player.id ? '©' : null}
-                  </Text>
-                  <Text style={[styles.playerNameSecondaryText, styles.pb5]}>
-                    {player.playing_position && player.playing_position.playing_position}
-                  </Text>
-                  {navigation.getParam('team').admin[0] === player.id
-                    ? (
-                      <Text style={[styles.playerNameSecondaryText, styles.pb5]}>
-                        Team Admin
+            {players && players.map((player) => {
+              const following = player.followers && player.followers.find(p => p.user.id === authPlayer.user.id);
+              return (
+                <TouchableOpacity key={player.id} style={{ paddingTop: 15, paddingBottom: 15, flex: 1, borderRightWidth: 0.6, borderRightColor: 'rgba(0,0,0,.3)' }} onPress={() => navigation.navigate(paths.client.ProfilesView, { id: player.id })}>
+                  <View style={styles.displayFlexCenterColumn}>
+                    <View style={styles.TeamsJoinCardImage}>
+                      <Thumbnail style={styles.joinTeamCardImage} source={player.user.profile_pic ? { uri: player.user.profile_pic } : images.user} />
+                    </View>
+                    <Text style={[styles.playerNameText, styles.py10]}>
+                      {player.user.first_name}
+                      {' '}
+                      {player.user.last_name}
+                      {navigation.getParam('team').captain === player.id ? '©' : null}
+                    </Text>
+                    <Text style={[styles.playerNameSecondaryText, styles.pb5]}>
+                      {player.playing_position && player.playing_position.playing_position}
+                    </Text>
+                    {navigation.getParam('team').admin[0] === player.id
+                      ? (
+                        <Text style={[styles.playerNameSecondaryText, styles.pb5]}>
+                          Team Admin
                       </Text>
-                    )
-                    : null
-                  }
-                  <TouchableOpacity onPress={() => this.handleCreateRelationship(player.id)} style={player.followers && player.followers.indexOf(authPlayer.user.id) !== -1 ? styles.playerFollowingButton : styles.playerFollowButton}>
-                    <Text style={player.followers && player.followers.indexOf(authPlayer.user.id) !== -1 ? styles.playerFollowingButtonText : styles.playerFollowButtonText}>
-                      {player.followers && player.followers.indexOf(authPlayer.user.id) !== -1 ? 'Following' : 'Follow'}
+                      )
+                      : null
+                    }
+                    <TouchableOpacity onPress={() => this.handleCreateRelationship(player.id)} style={following ? styles.playerFollowingButton : styles.playerFollowButton}>
+                      <Text style={following ? styles.playerFollowingButtonText : styles.playerFollowButtonText}>
+                        {following ? 'Following' : 'Follow'}
+                      </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.logout} style={styles.playerFollowButton}>
+                      <Text style={styles.playerFollowButtonText}>
+                        Logout
                     </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={this.logout} style={styles.playerFollowButton}>
-                    <Text style={styles.playerFollowButtonText}>
-                      Logout
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-              </TouchableOpacity>
-            ))}
+                    </TouchableOpacity>
+                  </View>
+                </TouchableOpacity>
+              )
+            })}
           </View>
         </Content>
         <View style={[styles.footer]}>
