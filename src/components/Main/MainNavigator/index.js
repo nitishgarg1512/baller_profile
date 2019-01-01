16390692295db5b344ac334e1fef2cb9257122ff
nitish.garg1512 <nitish.garg1512@gmail.com>
2019-01-01 16:36:29 +0700

    update menus

--- src/components/Main/MainNavigator/index.js
+++ src/components/Main/MainNavigator/index.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
+import { Image } from 'react-native';
 import { Button, Text, Icon, Footer, FooterTab } from 'native-base';
 
 import MatchFeed from '../MatchFeed';
 import Notification from '../Notification';
 import Stats from '../Stats';
 import Team from '../Team';
+
+import styles from './styles';
+
+const matchfeed = require('../../../../../static/images/matchfeed.png');
+const team = require('../../../../../static/images/teams.png');
+const notification = require('../../../../../static/images/notification.png');
+const reward = require('../../../../../static/images/reward.png');
+const menu = require('../../../../../static/images/menu.png');
 
 const MainNavigator = createBottomTabNavigator(
   {
@@ -23,26 +32,26 @@ const MainNavigator = createBottomTabNavigator(
             <Button
               active={index === 0}
               onPress={() => props.navigation.navigate('Team')}>
-              <Icon type="FontAwesome" name="users" />
+              <Image source={team} style={styles.icoStyle} />
             </Button>
             <Button
               active={index === 1}
               onPress={() => props.navigation.navigate('Stats')}>
-              <Icon type="FontAwesome" name="trophy" />
+              <Image source={reward} style={styles.icoStyle} />
             </Button>
             <Button
               active={index === 2}
               onPress={() => props.navigation.navigate('MatchFeed')}>
-              <Icon type="FontAwesome" name="address-card" />
+              <Image source={matchfeed} style={styles.icoStyle} />
             </Button>
             <Button
               active={index === 3}
               onPress={() => props.navigation.navigate('Notification')}>
-              <Icon type="FontAwesome" name="bell" />
+              <Image source={notification} style={styles.icoStyle} />
             </Button>
             <Button
               active={index === 4}>
-              <Icon name="menu" />
+              <Image source={menu} style={styles.icoStyle} />
             </Button>
           </FooterTab>
         </Footer>
---
+++ src/components/Main/MainNavigator/styles/index.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default {
+  icoStyle: {
+    width: 30,
+    height: 30,
+  },
+};
