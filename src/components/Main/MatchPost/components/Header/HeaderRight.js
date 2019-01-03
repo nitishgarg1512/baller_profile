e88c1f43cbf56c4d660477e9f91f288588298d0c
nitishgarg1512 <nitish.garg1512@gmail.com>
2019-01-03 21:09:57 +0700

    initial MatchPost

--- src/components/Main/MatchPost/components/Header/HeaderRight.js
+++ src/components/Main/MatchPost/components/Header/HeaderRight.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, Text, StyleSheet } from 'react-native';
 import { Container, Header } from 'native-base';
 
 class HeaderRight extends Component {
 
     render() {
-        const { textStyle } = styles;
         return (
-            <Text style={textStyle}>{this.props.format_of_match}</Text>
+            <Button title={this.props.format_of_match} color="#fff" />
         )
     }
 }
