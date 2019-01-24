0c049944542223753757c90b7f67347e68aecaeb
nitishgarg1512 <nitish.garg1512@gmail.com>
2019-01-24 16:31:08 +0700

    C-Stats: Add stats content.

--- src/components/Main/MatchPost/components/Tabs/MatchStats/styles/index.js
+++ src/components/Main/MatchPost/components/Tabs/MatchStats/styles/index.js
@@ -58,15 +58,117 @@ export default {
   playerImage: {
     alignItems: 'flex-start',
     justifyContent: 'center',
-    height: 40,
-  	width: 40,
+    height: 34,
+  	width: 34,
   },
   teamLogo: {
-  	height: 40,
+  	height: 34,
   	resizeMode: 'contain'
   },
   team2BadgeLogoView: {
   	flex: 1.5,
   	alignItems: 'flex-start',
   },
+  statsView: {
+    paddingBottom: 20
+  },
+  statsTeam: {
+    paddingTop: 10,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap:'wrap',
+  },
+  statsTeam1Logo: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  statsTeam1Image: {
+    height: 40,
+    width: 40,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
+  statsTeamTitle: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  statsTeam1TitleText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    fontFamily: 'calibri-italic',
+    color: '#3f3f3f',
+    justifyContent: 'center',
+  },
+  statsTeam2Logo: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  statsTeam2Image: {
+    height: 40,
+    width: 40,
+    resizeMode: 'contain',
+    justifyContent: 'center',
+  },
+  statsBpPoint: {
+    paddingTop: 18,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap:'wrap',
+  },
+  statsTeam1BpPoint: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  statsBpPointNumber: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontFamily: 'calibri-italic',
+    color: '#3f3f3f',
+    fontWeight: 'bold'
+  },
+  statsBpPointTitle: {
+    alignItems: 'center',
+    flex: 2,
+  },
+  statsBpPointTitleText: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontFamily: 'calibri-italic',
+    color: '#3f3f3f',
+    fontWeight: 'bold'
+  },
+  statsTeam2BpPoint: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  statsPointList: {
+    paddingTop: 16,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap:'wrap',
+  },
+  statsTeam1Point: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  statsPointNumber: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontFamily: 'calibri-italic',
+    color: '#3f3f3f',
+  },
+  statsPointTitle: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  statsPointTitleText: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    fontFamily: 'calibri-italic',
+    color: '#3f3f3f',
+  },
+  statsTeam2Point: {
+    flex: 1,
+    alignItems: 'flex-start',
+  }
 };
